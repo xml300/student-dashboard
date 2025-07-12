da9dd13b888655d3f08f0d9eb67645612934becd
xml300 <timiowoeye@gmail.com>
2025-07-12 22:30:26 +0100

    Changes to be committed: 	modified:   .gitignore 	modified:   package.json 	modified:   tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  darkMode: ["class", '[data-theme="dark"]'],
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        'card-background': 'var(--card-background)',
-        'primary-accent': 'var(--primary-accent)',
-        'secondary-accent': 'var(--secondary-accent)',
-        'border-color': 'var(--border-color)',
-        'accent': 'var(--accent)',
-      },
-      fontFamily: {
-        sans: ['var(--font-sans)', 'sans-serif'],
-=======
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -30,5 +6,4 @@ const config: Config = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ]
 }
-export default config
->>>>>>> 89af23dbb9007f91ba737715a825608d4b6da105
+export default config;
