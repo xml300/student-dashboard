36343a7581e2e4d1101cf9ca93320b42cb3feffe
xml300 <33401157+xml300@users.noreply.github.com>
2025-07-17 09:54:13 +0100

    Update page.tsx

--- src/app/(main)/attendance/[sessionId]/page.tsx
+++ src/app/(main)/attendance/[sessionId]/page.tsx
@@ -100,7 +100,8 @@ export default function AttendancePage() {
       const CHARACTERISTIC_UUID = "bfc0c92f-317d-4ba9-976b-cc11ce77b4ca";
       alert(JSON.stringify(SERVICE_UUIDs))
       console.log(SERVICE_UUIDs)
-      device = await navigator.bluetooth.requestDevice({ filters: [{ services: SERVICE_UUIDs }] });
+      //device = await navigator.bluetooth.requestDevice({ filters: [{ services: SERVICE_UUIDs }] });
+      device = await navigator.bluetooth.requestDevice({acceptAllDevices: true});
       const server = await device.gatt?.connect();
       if (!server) throw new Error("Couldn't connect to device");
 
